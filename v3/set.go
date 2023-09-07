3faf2c2eea7377249fb63b8e084759dff13fe614
Swati Modi <swatimodi1609@gmail.com>
2023-09-08 01:55:39 +0530

    set with rwmutex for separate read and write

---
+++ v3/set.go
@@ -0,0 +1,45 @@
+package v3
+
+import (
+	"gosetgo/ds"
+	"sync"
+)
+
+type set struct {
+	items map[interface{}]bool
+	mu    sync.RWMutex
+}
+
+func NewSet() ds.Set {
+	return &set{
+		items: make(map[interface{}]bool),
+		mu:    sync.RWMutex{},
+	}
+}
+
+func (s *set) Add(item interface{}) bool {
+	if s.Contains(item) {
+		return false
+	}
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.items[item] = true
+	return true
+}
+
+func (s *set) Remove(item interface{}) bool {
+	if !s.Contains(item) {
+		return false
+	}
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	delete(s.items, item)
+	return true
+}
+
+func (s *set) Contains(item interface{}) bool {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	_, ok := s.items[item]
+	return ok
+}
